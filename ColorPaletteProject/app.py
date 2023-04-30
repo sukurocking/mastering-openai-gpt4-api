fefa15ca8a01c468af99a773f389464789da77fd
Sukumar Subudhi <suku.subudhi@gmail.com>
2023-04-30 16:59:01 +0530

    logical pause to color palette project

--- ColorPaletteProject/app.py
+++ ColorPaletteProject/app.py
@@ -51,11 +51,7 @@ def prompt_to_palette():
 
 @app.route("/")
 def index():
-    response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt="Give me a funny word: "
-    )
-    return response["choices"][0]["text"]
+    return render_template("index.html")
     # return render_template("index.html")
 
 # @app.route("/dog")
